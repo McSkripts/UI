81f84060e511397c060dc9383d87a507afb72507
Mikkel S. Vestergård <mikkel@mikkelsv.dk>
2022-04-17 10:45:17 +0200

    created member view design

--- src/components/partials/members/members.view.tsx
+++ src/components/partials/members/members.view.tsx
@@ -17,8 +17,15 @@ function MemberView(args? : Args) {
     <Row>
       {args?.Users && args?.Users.map((user, index) => (
         <Col as={Link} to={`/member/${user.Id}`} sm={{ span: 6 }} md={{ span: 4 }} lg={{ span: 3 }} key={index} className="text-decoration-none">
-          <Card body className="mb-3">
-            {JSON.stringify(user)}
+          <Card className="mb-3">
+            <Card.Body className="p-0">
+              <div className="member-banner" style={{backgroundImage: `url("https://mcskri.pt${user.Banner.File.Location}${user.Banner.File.Name}.${user.Banner.File.Type}")`}} />
+              <div className="rect-img-container member-avatar">
+                <img className="rect-img" src={`https://mcskri.pt${user.Avatar.File.Location}${user.Avatar.File.Name}.${user.Avatar.File.Type}`} />
+              </div>
+              <p className="mt-3 h5 text-center">{user.DisplayName}</p>
+              Stats
+            </Card.Body>
           </Card>
         </Col>
       ))}
